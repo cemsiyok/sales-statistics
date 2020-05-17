aae888efb778eeb5cbf9fe0a2ed6018d2a00747a
cem.siyok <cem.siyok@free-now.com>
2020-05-17 18:01:24 +0200

    remove model attribute

--- src/main/java/com/ebay/salesstatsservice/controller/SalesStatisticsController.java
+++ src/main/java/com/ebay/salesstatsservice/controller/SalesStatisticsController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,7 +26,7 @@ public class SalesStatisticsController {
 
     @PostMapping(value = "/sales", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public Mono<Void> createSale(@Validated @ModelAttribute SalesRequest salesRequest) {
+    public Mono<Void> createSale(@Validated SalesRequest salesRequest) {
         return salesStatisticsService.feed(salesRequest.getSales_amount());
     }
 
